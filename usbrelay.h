b894df8288f00d36236ce3a541cccb3fd64ebaf9
Igor Galarraga <igalarraga@tinkoa.com>
2016-07-11 11:24:01 +0200

    Applying indent:  indent usbrelay.h -kr -nut  indent usbrelay.c -kr -nut

--- usbrelay.h
+++ usbrelay.h
@@ -21,12 +21,12 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #define ON 0xff
 #define OFF 0xfd
 
-int operate_relay(hid_device *handle,unsigned char relay, unsigned char state);
+int operate_relay(hid_device * handle, unsigned char relay,
+                  unsigned char state);
 
-struct  relay {
-	char this_serial[10];
-	unsigned char relay_num;
-	unsigned char state;
-	int found;
+struct relay {
+    char this_serial[10];
+    unsigned char relay_num;
+    unsigned char state;
+    int found;
 };
-	
